c5476c0b32f651449c05b01b9d906e0c3f3bddec
Nik <niktoring77@gmail.com>
2021-03-13 06:15:53 +0200

    layout formation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,25 @@
 import {ExcelComponent} from "@core/ExcelComponent";
 
 export class Header extends ExcelComponent {
+    static className = 'excel__header'
 
+    toHtml() {
+        return `
+            <input type="text" class="input" value="Новая таблица">
+
+            <div>
+                <button class="button">
+                    <span class="material-icons">
+                        exit_to_app
+                    </span>
+                </button>
+                <button class="button">
+                    <span class="material-icons">
+                        delete
+                    </span>
+                </button>
+
+            </div>
+        `
+    }
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,5 +1,40 @@
 import {ExcelComponent} from "@core/ExcelComponent";
 
 export class Toolbar extends ExcelComponent{
+    static className = 'excel__toolbar'
 
+    toHtml() {
+        return `
+            <button class="button">
+                    <span class="material-icons">
+                        format_align_left
+                    </span>
+            </button>
+            <button class="button">
+                    <span class="material-icons">
+                        format_align_center
+                    </span>
+            </button>
+            <button class="button">
+                    <span class="material-icons">
+                        format_align_right
+                    </span>
+            </button>
+            <button class="button">
+                    <span class="material-icons">
+                        format_bold
+                    </span>
+            </button>
+            <button class="button">
+                    <span class="material-icons">
+                        format_italic
+                    </span>
+            </button>
+            <button class="button">
+                    <span class="material-icons">
+                        format_underlined
+                    </span>
+            </button>
+        `
+    }
 }
